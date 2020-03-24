0a8e6747efc5c1a9e58819307d02d2f89464685a
fred62879 <zhuotingxie-fred@outlook.com>
2020-03-23 17:07:08 -0700

    "query builder transformation debugged 3rd"

--- src/rest/Server.ts
+++ src/rest/Server.ts
@@ -135,7 +135,8 @@ export default class Server {
         Log.trace("Server::post - params: " + req.body);
         try {
             // const query = JSON.parse(req.body);
-            const query = req.body; //
+            const query = req.body;
+            // const query = JSON.parse(req.body);
             Server.insight.performQuery(query).then((arr) => {
                 Log.info("Server::post(" + query + ") - responding 200");
                 res.json(200, { result: arr });
